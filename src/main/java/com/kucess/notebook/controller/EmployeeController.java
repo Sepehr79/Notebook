cf65e013ec39e5fae6297bc3fe86e423c92ce16f
Sepehr79 <sepehrmsm1379@gmail.com>
2022-02-24 17:13:55 +0330

    unit testing

--- src/main/java/com/kucess/notebook/controller/EmployeeController.java
+++ src/main/java/com/kucess/notebook/controller/EmployeeController.java
@@ -17,23 +17,26 @@ public class EmployeeController {
 
     private final EmployeeService employeeService;
 
+    private static final String EMPLOYEE = "employee";
+    private static final String ADMIN = "admin";
+
     @PostMapping("/admins/{userName}/employees")
     @Transactional
     public StatusResponse addEmployeeToAdmin(@RequestBody EmployeeIO employeeIO, @PathVariable String userName){
         employeeService.addEmployeeToAdmin(userName ,employeeIO);
-        return new StatusResponse(Message.EMPLOYEE_ADD_TOO_ADMIN, Map.of("adminUsername", userName, "employeeUsername", employeeIO.getUserName()));
+        return new StatusResponse(Message.EMPLOYEE_ADD_TOO_ADMIN, Map.of(ADMIN, userName, EMPLOYEE, employeeIO.getUserName()));
     }
 
     @PutMapping("/admins/{adminUserName}/employees/{employeeUserName}")
     public StatusResponse addCurrentEmployeeToAdmin(@PathVariable String adminUserName, @PathVariable String employeeUserName){
         employeeService.addEmployeeToAdmin(adminUserName, employeeUserName);
-        return new StatusResponse(Message.CURRENT_EMPLOYEE_ADD_TO_ADMIN,Map.of("adminUsername", adminUserName, "employeeUsername", employeeUserName));
+        return new StatusResponse(Message.CURRENT_EMPLOYEE_ADD_TO_ADMIN,Map.of(ADMIN, adminUserName, EMPLOYEE, employeeUserName));
     }
 
     @DeleteMapping("/admins/{adminUserName}/employees/{employeeUserName}")
     public StatusResponse removeEmployeeFromAdmin(@PathVariable String adminUserName, @PathVariable String employeeUserName){
         employeeService.removeEmployeeFromAdmin(adminUserName, employeeUserName);
-        return new StatusResponse(Message.EMPLOYEE_REMOVED_FROM_ADMIN, Map.of("adminUsername", adminUserName, "employeeUsername", employeeUserName));
+        return new StatusResponse(Message.EMPLOYEE_REMOVED_FROM_ADMIN, Map.of(ADMIN, adminUserName, EMPLOYEE, employeeUserName));
     }
 
     @GetMapping("/employees/{userName}")
@@ -41,11 +44,11 @@ public class EmployeeController {
         return employeeService.findEmployeeByUserName(userName);
     }
 
-    @PostMapping("/employees")
-    public StatusResponse changePassword(@RequestParam(name = "user") final String userName,
+    @PostMapping("/employees/{employeeUsername}")
+    public StatusResponse changePassword(@PathVariable String employeeUsername,
                                @RequestParam(name = "pass") final String password){
-        employeeService.changeEmployeePassword(userName, password);
-        return new StatusResponse(Message.EMPLOYEE_PASSWORD_CHANGE, Map.of("employeeUsername", userName));
+        employeeService.changeEmployeePassword(employeeUsername, password);
+        return new StatusResponse(Message.EMPLOYEE_PASSWORD_CHANGE, Map.of(EMPLOYEE, employeeUsername));
     }
 
 }
