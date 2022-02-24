9dd0c328fcb24181a62da4f0783e8b7445c7c22a
Sepehr79 <sepehrmsm1379@gmail.com>
2022-02-24 20:13:13 +0330

    tests refactoring

--- src/test/java/com/kucess/notebook/controller/AdminControllerTest.java
+++ src/test/java/com/kucess/notebook/controller/AdminControllerTest.java
@@ -12,18 +12,22 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(properties = "spring.datasource.url=jdbc:h2:mem:test3")
 @AutoConfigureMockMvc
 class AdminControllerTest {
+
+    private static final String JSON = "application/json";
 
     @Autowired
     private MockMvc mockMvc;
@@ -42,51 +46,64 @@ class AdminControllerTest {
                 .build();
 
         // Create new admin user
-        perform(post(URL)
-                .contentType("application/json")
-                .content(OBJECT_MAPPER.writeValueAsString(adminIO)))
-                .andExpect(status().isOk())
-                .andDo(result -> {
+        perform(
+                post(URL).contentType(JSON).content(OBJECT_MAPPER.writeValueAsString(adminIO)),
+                status().isOk(),
+                result -> {
                     StatusResponse response = OBJECT_MAPPER.readValue(result.getResponse().getContentAsString(), StatusResponse.class);
                     assertEquals(Message.ADMIN_REGISTERED, response.getMessage());
-                });
+                }
+        );
 
         // Update admin
-        perform(put(URL + "/CCC")
-                .contentType("application/json")
-                .content(OBJECT_MAPPER.writeValueAsString(adminIO.toBuilder().name("KUCESS").userName("sample").build())))
-                .andExpect(status().isOk());
+        perform(
+                put(URL + "/CCC").contentType("application/json")
+                        .content(OBJECT_MAPPER.writeValueAsString(adminIO.toBuilder().name("KUCESS").userName("sample").build())),
+                status().isOk(),
+                print()
+        );
 
         // Get admin with updated attributes
-        perform(get(URL + "/sample"))
-                .andExpect(status().isOk())
-                .andDo(result -> {
+        perform(
+                get(URL + "/sample"),
+                status().isOk(),
+                result -> {
                     AdminIO updated = OBJECT_MAPPER.readValue(result.getResponse().getContentAsString(), AdminIO.class);
                     assertEquals("KUCESS" ,updated.getName());
-                });
+                }
+        );
 
         // Delete admin
-        perform(delete(URL + "/sample"))
-                .andExpect(status().isOk())
-                .andExpect(result -> {
+        perform(
+                delete(URL + "/sample"),
+                status().isOk(),
+                result -> {
                     Map<String, String> properties = OBJECT_MAPPER.readValue(result.getResponse().getContentAsString(), StatusResponse.class)
                             .getProperties();
                     assertEquals("sample" ,properties.get("username"));
-                });
+                }
+        );
 
         // Username not found
-        perform(get(URL + "/sample"))
-                .andExpect(status().is4xxClientError())
-                .andDo(result -> {
+        perform(
+                get(URL + "/sample"),
+                status().isBadRequest(),
+                result -> {
                     ExceptionResponse exceptionResponse = OBJECT_MAPPER.readValue(result.getResponse().getContentAsString(), ExceptionResponse.class);
                     assertEquals(HttpStatus.BAD_REQUEST.name(), exceptionResponse.getResponseType());
-                });
+                }
+        );
 
     }
 
     @SneakyThrows
-    private ResultActions perform(MockHttpServletRequestBuilder mockHttpServletRequestBuilder){
-        return mockMvc.perform(mockHttpServletRequestBuilder);
+    private void perform(MockHttpServletRequestBuilder requestBuilder,
+                         ResultMatcher resultMatcher,
+                         ResultHandler resultHandler){
+        mockMvc.perform(requestBuilder)
+                .andExpect(resultMatcher)
+                .andDo(resultHandler);
+
     }
 
 }
