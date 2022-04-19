36ad17193a6571c1edf6b16295c0a4f588ee0c2d
Sepehr79 <sepehrmsm1379@gmail.com>
2022-04-19 11:26:03 +0430

    logging user details

--- src/main/java/com/kucess/notebook/security/SecurityEnableConfiguration.java
+++ src/main/java/com/kucess/notebook/security/SecurityEnableConfiguration.java
@@ -1,17 +1,23 @@
 package com.kucess.notebook.security;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
 @ConditionalOnProperty("security.enable")
+@Slf4j
 public class SecurityEnableConfiguration extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -33,8 +39,19 @@ public class SecurityEnableConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .mvcMatchers("/notebook/v1/home").authenticated()
                 .mvcMatchers("/notebook/v1/employees/**").hasAnyAuthority("ADMIN")
-                .mvcMatchers("/notebook/v1").permitAll()
                 .mvcMatchers("/signup").permitAll()
-                .antMatchers("/resources/templates/**").permitAll();
+                .and()
+                .addFilterAfter((request, response, chain) -> {
+                    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    if (!authentication.getName().equals("anonymousUser")) {
+                        log.info("User successfully authenticated with the username: {}\n authorities: {}", authentication.getName(), authentication.getAuthorities());
+                    }
+                    chain.doFilter(request, response);
+                } ,FilterSecurityInterceptor.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/notebook/v1", "/resources/templates/**");
     }
 }
