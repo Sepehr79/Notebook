8cf20a362952bd4299743839e746c5b2371504b0
Sepehr79 <sepehrmsm1379@gmail.com>
2022-04-26 17:57:44 +0430

    captcha configuration

---
+++ src/main/java/com/kucess/notebook/bussiness/captcha/dto/CaptchaResponse.java
@@ -0,0 +1,10 @@
+package com.kucess.notebook.bussiness.captcha.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class CaptchaResponse {
+    private boolean success;
+}
